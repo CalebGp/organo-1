ee5e271b89e8b0b2c45097279da0b49eb87b2cb8
CalebGp <caleb.gp.dev@gmail.com>
2024-02-17 13:53:51 +0000

    Finished!

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,39 @@
-import './colaborador.css'
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import "./colaborador.css";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, mudarFavotito }) => {
+  const propsFavorito = {
+    size: 25,
+    onClick: (e) => {
+      e.preventDefault();
+      mudarFavotito();
+    },
+  };
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle
+        size={25}
+        onClick={(e) => {
+          aoDeletar(colaborador.id);
+        }}
+        className="deletar"
+      />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito ? (
+            <AiFillHeart {...propsFavorito} color="#ff0000" />
+          ) : (
+            <AiOutlineHeart {...propsFavorito} />
+          )}
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,13 +1,28 @@
-import './lista-suspensa.css'
+import "./lista-suspensa.css";
+import { v4 } from "uuid";
 
-const ListaSuspensa = ({label, items,  valor, aoAlterado, obrigatorio = false}) => {
-    return (<div className="lista-suspensa">
-        <label>{label}</label>
-        <select required={obrigatorio} value={valor} onChange={evento => aoAlterado(evento.target.value)}>
-            <option />
-            {items.map(item => <option key={item}>{item}</option>)}
-        </select>
-    </div>)
-}
+const ListaSuspensa = ({
+  label,
+  items,
+  valor,
+  aoAlterado,
+  obrigatorio = false,
+}) => {
+  return (
+    <div className="lista-suspensa" key={label}>
+      <label>{label}</label>
+      <select
+        required={obrigatorio}
+        value={valor}
+        onChange={(evento) => aoAlterado(evento.target.value)}
+      >
+        <option />
+        {items.map((item) => (
+          <option key={v4()}>{item}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-export default ListaSuspensa
+export default ListaSuspensa;
